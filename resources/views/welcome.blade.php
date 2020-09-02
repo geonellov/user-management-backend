94e8fa619fe66bb2594ffae0f3baa6e679a8d805
geonellov <geonell@oostvoort.com>
2020-09-02 08:56:04 +0800

    backend commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+    <div class="w-100 d-flex flex-column justify-content-center align-items-center">
+       <user-component></user-component>
+    </div>
+@endsection
